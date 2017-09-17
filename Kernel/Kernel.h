e783d3f8909ea9aa57aa13e4465e9e3020183f4a
Smartheads <rohu747@gmail.com>
2017-09-17 14:34:22 +0200

    Update Kernel.h

--- Kernel/Kernel.h
+++ Kernel/Kernel.h
@@ -1,5 +1,28 @@
 #ifndef SMTKernel
 #define SMTKernel
 
+#include <RBD_Motor.h>
+#include <RBD_Timer.h>
+#include <MPU6050.h>
+#include <I2Cdev.h>
+#include <Newping.h>
+#include <vector.h>
+#include <Arduino.h>
+
+typedef namespace SmartKernelForRover {
+  int baud;
+  motorList mList;
+  vector<int> usedPins;
+  
+  // Kernel functions
+  void begin(int b = 115200);
+  void loop(void);
+  
+  struct motorList;
+  
+  // Driver implementations
+  
+  
+} SMTK;
 
 #endif
