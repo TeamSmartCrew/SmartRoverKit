5307138ca48d6d071617d1d3e29f28e0fde6e65b
Smartheads <rohu747@gmail.com>
2017-09-17 14:00:42 +0200

    Update Kernel.cpp
    Added implementation for kernel start and kernel update. Added a new link list storing all motors.

--- Kernel/Kernel.cpp
+++ Kernel/Kernel.cpp
@@ -1,2 +1,47 @@
 #include "Arduino.h"
 #include "Kernel.h"
+#include <RBD_Motor.h>
+#include <RBD_Timer.h>
+#include <MPU6050.h>
+#include <I2Cdev.h>
+#include <Newping.h>
+
+/*
+* Starts the kernel
+* @param int baud rate for serial, 
+*/
+void SMTK::begin(int b = 115200)
+{
+  SMTK::baud = b;
+  Serial.begin(baud);
+  
+  Serial.println("Started SMTK kernel. v.1.0.0 Alpha");
+}
+
+/*
+* Update the kernel upon every tick
+*,
+* @param void
+*/
+void SMTK::loop(void)
+{
+  // Update all motors
+  if (mList.next != null)
+  {
+    motorList ml;
+    ml.motor = mList.motor;
+    ml.next = mList.next;
+    while (ml.next != null)
+    {
+      ml.motor->update();
+      
+      ml = *ml.next;
+    }
+  }
+}
+
+struct SMTK::motorList
+{
+  RBD::Motor* motor;
+  const SMTK::motorList* next;
+}
